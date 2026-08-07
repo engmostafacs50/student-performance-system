1881db7d0b0cf713941265475743b217b8a6ddc7
most3f3.1112006@gmail.com <most3f3.1112006@gmail.com>
2026-08-07 03:31:47 +0300

    add student functionality

---
+++ student.py
@@ -0,0 +1,18 @@
+class student : 
+    def __init__ (self,id,name,department,math,physics,english) : 
+        self.__ID = id
+        self.__name = name 
+        self.__department = department 
+        self.__math = math 
+        self.__physics = physics
+        self.__english = english 
+    
+    def to_dict(self) : 
+        {
+            "ID" : self.__ID , 
+            "Name" : self.__name , 
+            "Department" : self.__department , 
+            "Math" : self.__math , 
+            "Physics" : self.__physics , 
+            "English" : self.__english 
+        }
